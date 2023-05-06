9ff89b6cff83eb326afcb9fc9b04c19d275fc168
Melike Sinem Kaya <msinemkaya6@gmail.com>
2023-05-06 20:59:52 +0300

    feat: created a context and wrapped the App.js with it

---
+++ src/context/books.js
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+// we have created a context with the name of BookContext
+// to use context you first should create it as a seperate file and export it
+// for all the components to be able to use it (access to it)
+export const BookContext = createContext()
--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BookContext } from './context/books';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  {/*
+     we could have done this inside of the App.js too as the parent (wrapper) on the return value 
+     it doesnt really matter. The thing that is important is that we usually wrap the App component with
+     context provider
+
+     Context provider is the second step of using context. We are providing the data
+     to all the childs (App component and what is inside of it) for them to use it.
+  */}
+    <BookContext.Provider value={''} >
+      <App />
+    </BookContext.Provider>
   </React.StrictMode>
 );
 
