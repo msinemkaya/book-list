11d5c8097755ac4afb33930773871a430b0aab00
Melike Sinem Kaya <msinemkaya6@gmail.com>
2023-05-06 22:55:33 +0300

    feat: refactored BookList using context

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,19 +2,23 @@ import BookShow from './BookShow'
 import { useContext } from 'react';
 import { BookContext } from '../context/books';
 
-export default function BookList({books, onDelete, onEdit}){
+export default function BookList(){
+    // WE NO LONGER NEED TO DO THAT
     // passes down the onDelete and onEdit to be used in BookShow
 
     // this is the 3rd step to use context. We say that we will use the bookContext in this component
     // as a variable called value (we pass the BookContext object to the value variable)
-    const value = useContext(BookContext)
+        // const value = useContext(BookContext)
+
+    // we take only the books from BookContext
+    const {books} = useContext(BookContext)
 
     //maps the books that has been sent and creates a BookShow component for each one of them
     // ALSO passes down the book itself to to be used for its title and id
     return(
         <div className='book-list'>
             {books.map((book) => (
-                <BookShow book={book} key={book.id} onDelete={onDelete} onEdit={onEdit} />
+                <BookShow book={book} key={book.id} />
             ))}
         </div>
     );
